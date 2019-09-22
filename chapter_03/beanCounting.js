137963b99b4f19707cf2313f78c82f8faec21363
jm-avila <jm.avila1309@icloud.com>
2019-09-22 13:08:57 +0200

    Bean counting solution

---
+++ chapter_03/beanCounting.js
@@ -0,0 +1,10 @@
+/*
+Bean counting
+Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters there are in the string.
+
+Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
+*/
+
+function countChar(str, char) {
+  return str.split("").filter(item => item == char).length;
+}
